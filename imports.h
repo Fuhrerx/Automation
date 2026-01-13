b4d7dd0f4ab71e0bde81001cb49319cfa085b8a3
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-13 14:58:03 +0530

    more changes

--- imports.h
+++ imports.h
@@ -19,6 +19,26 @@ char sns[25] = "import seaborn as sns";
 char sk_lin[50] = "from sklearn.linear_model import LinearRegression";
 char sk_lin_ridge[50] = "from sklearn.linear_model import Ridge";
 
+// log regression
+
+char sk_log[60] = "from sklearn.linear_model import LogisticRegression";
+
+// decision tree class
+
+char sk_dec_class[50] = "from sklearn.tree import DecisionTreeClassifier";
+
+// decision tree regressor
+
+char sk_dec_reg[50] = "from sklearn.tree import DecisionTreeRegressor";
+
+// XGBoost class
+
+char xgboost_class[50] = "from xgboost import XGBClassifier";
+
+// XGBoost regressor
+
+char xgboost_reg[50] = "import xgboost as xgb";
+
 void lower(char *str);
 
 void lower(char *str)
--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 FILE *fp;
 
 void ask_sns();
+void ask_model();
 
 int main()
 {
@@ -53,6 +54,7 @@ int main()
         fprintf(fp, "\n");
 
         ask_sns();
+        ask_model();
 
         printf("File written successfully at %s\n", relative_path);
         fclose(fp);
@@ -74,5 +76,51 @@ void ask_sns()
     {
         fprintf(fp, sns);
         fprintf(fp, "\n");
+    }
+}
+
+void ask_model()
+{
+    int ans1;
+
+    printf("What model are you planning to use : \n");
+    printf("Linear Regresssion (1)\nLogistic Regression (2)\nDecision Trees classifier (3)\nDecision Tree regressor (4)\nXGBoost classifier (5)\nXGBoost regressor (6)\n");
+    printf("Choose : ");
+    scanf("%d", &ans1);
+
+    switch (ans1)
+    {
+    case 1:
+        fprintf(fp, sk_lin);
+        fprintf(fp, "\n");
+        break;
+
+    case 2:
+        fprintf(fp, sk_log);
+        fprintf(fp, "\n");
+        break;
+
+    case 3:
+        fprintf(fp, sk_dec_class);
+        fprintf(fp, "\n");
+        break;
+
+    case 4:
+        fprintf(fp, sk_dec_reg);
+        fprintf(fp, "\n");
+        break;
+
+    case 5:
+        fprintf(fp, xgboost_class);
+        fprintf(fp, "\n");
+        break;
+
+    case 6:
+        fprintf(fp, xgboost_reg);
+        fprintf(fp, "\n");
+        break;
+
+    default:
+        printf("invalid");
     }
 }
