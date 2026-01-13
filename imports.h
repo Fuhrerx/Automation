9619283c55759f33fa9acb26c102721ccab9c614
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-13 12:45:16 +0530

    main and imports file

---
+++ imports.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+const char sns[25] = "import seaborn as sns";
+const char pd[25] = "import pandas as pd";
+const char np[25] = "import numpy as np";
+const char torch[25] = "import torch";
+const char plt[35] = "import matplotlib.pyplot as plt";
--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 #include <direct.h>
 #include <errno.h>
+#include <string.h>
+
+#include "imports.h"
 
 int main()
 {
+    FILE *fp;
     const char *dir_name = "python_automation";
+    const char *relative_path = "python_automation/main.ipynb";
     int result = _mkdir(dir_name);
 
     if (result == 0)
@@ -24,5 +29,20 @@ int main()
             perror("Error creating directory");
         }
     }
+
+    fp = fopen(relative_path, "w");
+
+    if (fp == NULL)
+    {
+        printf("Error opening file with relative path: %s\n", strerror(errno));
+    }
+    else
+    {
+        fclose(fp);
+        printf("File written successfully at %s\n", relative_path);
+    }
+
+    fprintf(fp, sns);
+
     return 0;
 }
