7e6d03ed722f21b01c4c36ab10c89a216beaa0e4
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-14 18:20:58 +0530

    start of model definition

---
+++ models.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <errno.h>
+#include <string.h>
+
+#include "imports.h"
+#include "utils.h"
