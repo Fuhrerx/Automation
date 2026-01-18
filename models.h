476efa550bbe87fe977488737b4cb9bde8186e84
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-18 17:23:08 +0530

    models.h - implimented all regression models

--- models.h
+++ models.h
@@ -38,3 +38,120 @@ void r_lin_reg()
     fprintf(fp, "y_pred = model.predict(X_train)\n");
     fprintf(fp, "y_pred\n");
 }
+
+void r_log_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = LogisticRegression()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_dec_tree_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = DecisionTreeRegressor()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_xgbooost_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = xgb.XGBRegressor()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_lin_ridge_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = Ridge()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_ran_for_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = RandomForestRegressor()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_k_nearest_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = KNeighborsRegressor()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_lasso_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = Lasso()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_elastic_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = ElasticNet()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
+
+void r_svr_reg()
+{
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "model = SVR()\n");
+    fprintf(fp, "model.fit(X_train, y_train)\n");
+    fprintf(fp, "model.coef_\n");
+    fprintf(fp, CELL);
+    fprintf(fp, "\n");
+    fprintf(fp, "y_pred = model.predict(X_train)\n");
+    fprintf(fp, "y_pred\n");
+}
