5a831751a8bac7dc764aa49a4fba7f9baff845bd
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-24 17:38:09 +0530

    main.c - prep for app building

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 // issues.
 //
 
+#pragma once
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -15,7 +17,67 @@
 
 #include "utils.h"
 
+extern int app(); // TODO : use only for app creation
+
 int main(int argc, char *argv[])
+{
+    // ask_dir_name(); // TODO : ADD CUSTOM NAME AND PATH
+    // ask_dir_path();
+    dir_check();
+
+    if (fp == NULL)
+    {
+        printf("\nError opening file with relative path: %s\n", strerror(errno));
+        printf("\n");
+
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        m_libs();
+
+        base_imports();
+        ask_torch();
+        ask_model();
+
+        m_read_csv();
+
+        read_csv_file();
+        drop();
+
+        m_plot();
+
+        hist();
+        corr();
+        heatmap();
+        pairplot();
+
+        log_transform();
+
+        m_feature_scaling();
+
+        std_scaler();
+
+        m_model_build();
+
+        model_build();
+
+        m_model_evaluation();
+
+        model_eval();
+
+        printf("\n");
+        printf("File written successfully at %s", relative_path);
+        printf("\n");
+
+        fclose(fp);
+
+        return EXIT_SUCCESS;
+    }
+    return 0;
+}
+
+extern int app()
 {
     // ask_dir_name(); // TODO : ADD CUSTOM NAME AND PATH
     // ask_dir_path();
