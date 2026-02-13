0123ac529570cf87c6d8586734c8b05e355cb0ed
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-13 09:10:49 +0530

    MLAuto.c - CLI application script

---
+++ App/MLAuto.c
@@ -0,0 +1,13 @@
+//
+// Main CLI application script, execute this file in any code editor/IDE or use the "MLAuto.exe"
+// in this directory as it is from file manager.
+//
+
+#include <stdio.h>
+
+#include "app.h"
+
+int main(int argc, char *argv[])
+{
+    app();
+}
