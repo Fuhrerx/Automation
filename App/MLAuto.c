35fc01b1bb86644a84c8661a172a961130d76bdd
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-15 16:22:08 +0530

    MLAuto.c - Added dev comments

--- App/MLAuto.c
+++ App/MLAuto.c
@@ -2,6 +2,14 @@
 // Main CLI application script, execute this file in any code editor/IDE or use the "MLAuto.exe"
 // in this directory as it is from file manager.
 //
+// To devs :
+// Run this code only after running main.c and validating all new features or fixes are fully
+// working as intended!
+//
+// Also run this code only during :
+//          * New feature/security updates
+//          * Feature rollbacks
+//
 
 #include <stdio.h>
 
