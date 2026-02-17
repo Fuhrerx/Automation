7b2a9005968a153c2950853827bf839fd3b8eaf9
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-17 17:14:25 +0530

    imports.h - Changed all imports to const

--- scikit/imports.h
+++ scikit/imports.h
@@ -9,79 +9,79 @@
 
 // base
 
-char pd[25] = "import pandas as pd";
-char np[25] = "import numpy as np";
-char torch[25] = "import torch";
-char plt[35] = "import matplotlib.pyplot as plt";
-char tts[55] = "from sklearn.model_selection import train_test_split";
-char as[60] = "from sklearn.metrics import accuracy_score, f1_score";
-char sc[50] = "from sklearn.preprocessing import StandardScaler";
+const char pd[25] = "import pandas as pd";
+const char np[25] = "import numpy as np";
+const char torch[25] = "import torch";
+const char plt[35] = "import matplotlib.pyplot as plt";
+const char tts[55] = "from sklearn.model_selection import train_test_split";
+const char as[60] = "from sklearn.metrics import accuracy_score, f1_score";
+const char sc[50] = "from sklearn.preprocessing import StandardScaler";
 
 // optional
 
-char sns[25] = "import seaborn as sns";
-char mse_r2[60] = "from sklearn.metrics import mean_squared_error, r2_score";
+const char sns[25] = "import seaborn as sns";
+const char mse_r2[60] = "from sklearn.metrics import mean_squared_error, r2_score";
 
 // linear regression
 
-char sk_lin[50] = "from sklearn.linear_model import LinearRegression";
+const char sk_lin[50] = "from sklearn.linear_model import LinearRegression";
 
 // ridge
 
-char sk_lin_ridge[50] = "from sklearn.linear_model import Ridge";
+const char sk_lin_ridge[50] = "from sklearn.linear_model import Ridge";
 
 // log regression
 
-char sk_log[60] = "from sklearn.linear_model import LogisticRegression";
+const char sk_log[60] = "from sklearn.linear_model import LogisticRegression";
 
 // decision tree class
 
-char sk_dec_class[50] = "from sklearn.tree import DecisionTreeClassifier";
+const char sk_dec_class[50] = "from sklearn.tree import DecisionTreeClassifier";
 
 // decision tree regressor
 
-char sk_dec_reg[50] = "from sklearn.tree import DecisionTreeRegressor";
+const char sk_dec_reg[50] = "from sklearn.tree import DecisionTreeRegressor";
 
 // XGBoost
 
-char xgboost[50] = "import xgboost as xgb";
+const char xgboost[50] = "import xgboost as xgb";
 
 // random forest class
 
-char ran_for_class[60] = "from sklearn.ensemble import RandomForestClassifier";
+const char ran_for_class[60] = "from sklearn.ensemble import RandomForestClassifier";
 
 // random forest regressor
 
-char ran_for_reg[50] = "from sklearn.ensemble import RandomForestRegressor";
+const char ran_for_reg[50] = "from sklearn.ensemble import RandomForestRegressor";
 
 // k means clustering
 
-char k_means_cluster[50] = "from sklearn.cluster import KMeans";
+const char k_means_cluster[50] = "from sklearn.cluster import KMeans";
 
 // k near neighbors class
 
-char k_near_class[50] = "from sklearn.neighbors import KNeighborsClassifier";
+const char k_near_class[50] = "from sklearn.neighbors import KNeighborsClassifier";
 
 // k near neighbors regressor
 
-char k_near_reg[50] = "from sklearn.neighbors import KNeighborsRegressor";
+const char k_near_reg[50] = "from sklearn.neighbors import KNeighborsRegressor";
 
 // lasso
 
-char lasso[50] = "from sklearn.linear_model import Lasso";
+const char lasso[50] = "from sklearn.linear_model import Lasso";
 
 //  elastic net
 
-char elastic[50] = "from sklearn.linear_model import ElasticNet";
+const char elastic[50] = "from sklearn.linear_model import ElasticNet";
 
 // svr
 
-char svr[30] = "from sklearn.svm import SVR";
+const char svr[30] = "from sklearn.svm import SVR";
 
 // svm
 
-char svm[40] = "from sklearn.svm import SVC";
+const char svm[40] = "from sklearn.svm import SVC";
 
 // mlp
 
-char mlp[50] = "from sklearn.neural_network import MLPClassifier";
+const char mlp[50] = "from sklearn.neural_network import MLPClassifier";
