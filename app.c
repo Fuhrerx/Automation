ec9fd9f6083918ad8f96bab7125eb7979173810e
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-25 17:40:08 +0530

    app.c - New app.c for main.so creation (for unix/linux)

---
+++ app.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <errno.h>
+#include <string.h>
+
+#include "structure/utils.h"
+
+extern int app();
+
+extern int app()
+{
+    // ask_dir_name(); // TODO : ADD CUSTOM NAME AND PATH
+    // ask_dir_path();
+    start_prog();
+
+    dir_check();
+
+    if (fp == NULL)
+    {
+        printf("\nError opening file with relative path: %s\n", strerror(errno));
+        printf("\n");
+
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        m_libs();
+
+        base_imports();
+        ask_torch();
+        ask_model();
+
+        m_read_csv();
+
+        read_csv_file();
+        drop();
+
+        m_plot();
+
+        hist();
+        corr();
+        heatmap();
+        pairplot();
+
+        log_transform();
+
+        m_feature_scaling();
+
+        std_scaler();
+
+        m_model_build();
+
+        model_build();
+
+        m_model_evaluation();
+
+        model_eval();
+
+        printf("\nFile written successfully at %s\n", relative_path);
+
+        fclose(fp);
+
+        return EXIT_SUCCESS;
+    }
+    return 0;
+}
