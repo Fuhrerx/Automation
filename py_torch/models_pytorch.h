d029a76d993243e18defa2e7cd5d9d0be47164f5
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-26 16:04:39 +0530

    models_pytorch.h - New Pytorch imports file

---
+++ py_torch/models_pytorch.h
@@ -0,0 +1,12 @@
+//
+// PyTorch module for upcoming PyTorch functions. Use pytorch_(func_name) for functions here.
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <errno.h>
+#include <string.h>
