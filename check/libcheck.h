fb9dc65ea72398dffa7ab12c023481ebdc927d7c
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-04 19:16:09 +0530

    libcheck.h - New library check file for debug

---
+++ check/libcheck.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <errno.h>
+#include <string.h>
+
+#include "../scikit/sk_imports.h"
+#include "../structure/cells.h"
+#include "../structure/PyList.h"
+#include "../structure/utils.h"
+
+int debug();
+
+int debug()
+{
+
+    return 0;
+}
