316da5cafe29fef0cd51b7630a0a979ed53ddd6d
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-05 18:56:10 +0530

    MLAuto.c - Fixed the new Debug mode

--- App/MLAuto.c
+++ App/MLAuto.c
@@ -14,8 +14,60 @@
 #include <stdio.h>
 
 #include "app.h"
+#include "../check/libcheck.h"
 
 int main(int argc, char *argv[])
 {
-    app();
+    if (argc == 1)
+    {
+        printf("\n>> Welcome to MLAuto CLI!\n");
+        printf("\n>> Usage : \t./MLAuto [-flag]\n");
+        printf("\n>> For Flag info, use [-h] flag.\n");
+
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp(argv[1], "-h") == 0)
+    {
+        printf("\n>> Help :\n");
+        printf("\n>> [-h]\t\tShow this help section\n");
+        printf("\n>> [-v]\t\tShow version info\n");
+        printf("\n>> [-d]\t\tDebug mode\n");
+        printf("\n>> [-w]\t\tWrite output to file\n");
+        printf("\n>> [-r]\t\tRead input from file\n");
+
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp(argv[1], "-v") == 0)
+    {
+        printf("\n>> MLAuto Version : %s\n", VER);
+        printf("\n>> Copyright (c) Akshay_Babu, All rights reserved!\n");
+
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp(argv[1], "-d") == 0)
+    {
+        printf("\n>> Debug mode activated!\n");
+        debug();
+
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp(argv[1], "-w") == 0)
+    {
+        app();
+
+        return EXIT_SUCCESS;
+    }
+    else if (strcmp(argv[1], "-r") == 0)
+    {
+        app();
+
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        printf("\n>> Invalid flag! Use [-h] for help.\n");
+
+        return EXIT_FAILURE;
+    }
+    return 0;
 }
