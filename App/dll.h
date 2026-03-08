69c78714309a7eb778a1592fcf6a4fd5324716b3
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-08 17:17:04 +0530

    dll.h - Changes to dll export functions

--- App/dll.h
+++ App/dll.h
@@ -1,7 +1,7 @@
 #ifdef BUILD_MY_DLL
-    #define MY_DLL_API __declspec(dllexport)
+#define MY_DLL_API __declspec(dllexport)
 #else
-    #define MY_DLL_API __declspec(dllimport)
+#define MY_DLL_API __declspec(dllimport)
 #endif
 
 MY_DLL_API int cli();
