c9bcd0d83bc392a2dac54906eea5ebb99b590296
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-03-09 16:25:30 +0530

    dll.h - Changed dev note format

--- App/dll.h
+++ App/dll.h
@@ -1,3 +1,9 @@
+/*
+    Essential header file for the DLL, contains the function declarations and the macro for exporting/importing
+    functions from the DLL.
+    Add new function declarations here if you add any new functions in the app.c file that you want to export.
+*/
+
 #ifdef BUILD_MY_DLL
 #define MY_DLL_API __declspec(dllexport)
 #else
